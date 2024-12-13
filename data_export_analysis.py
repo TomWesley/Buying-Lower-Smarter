461687fce8dfa85463d43ceeff0a280b50185559
ioanacardei <ioana.cardei@gmail.com>
2024-12-13 16:58:09 -0500

    Ioana's first changes

---
+++ data_export_analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import yfinance as yf
+import numpy as py
+from datetime import timedelta
+
+sp500_symbols = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]['Symbol'].tolist()
+sp500_symbols
