4bcaae3c46a01d3ae06119a72478eb9021acb661
Thomas Wesley <tomjwesley@gmail.com>
2025-02-21 09:08:49 -0500

    cleaned up comments, updated yahoo finance

--- main.py
+++ main.py
@@ -10,31 +10,31 @@ import time
 
 # Set the precise 5-year window ending 2 years ago
 # Set the precise 5-year window ending 2 years ago
-end_date = datetime.now() - timedelta(days=365*2)
-start_date = end_date - timedelta(days=365*35)
+end_date = datetime.now() - timedelta(days=365*5)
+start_date = end_date - timedelta(days=365*20)
 
 
-# Make both start_date and end_date timezone-aware (UTC)
+# # Make both start_date and end_date timezone-aware (UTC)
 ny_tz = pytz.timezone('America/New_York')
 end_date = ny_tz.localize(end_date.replace(hour=0, minute=0, second=0, microsecond=0))
 start_date = ny_tz.localize(start_date.replace(hour=0, minute=0, second=0, microsecond=0))
 
+
 def main():
     
-    #May need the link to the current symbols for the daily listener. 
-    #sp500_symbols = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]['Symbol'].tolist()
+    #Will update the SANDP symbols over time as the list changes 
     sp500_symbols = pd.read_csv('currentSANDP.csv', header=None).squeeze().tolist()
     data = {}
     for symbol in sp500_symbols:
         try:
             stock = yf.Ticker(symbol)
-            # Fetch historical data within the specific 5-year window
+            # Fetch historical data within the specific time window
             hist_data = stock.history(start=start_date, end=end_date)
-            
-            # Store the filtered data
+
+            #Store the filtered data
             if not hist_data.empty:
                 data[symbol] = hist_data
-                #print(f"Running {symbol}: data IS available in time window")
+                # print(f"Running {symbol}: data IS available in time window")
             else:
                 print(f"Skipping {symbol}: No data available in time window")
         except Exception as e:
@@ -46,9 +46,6 @@ def main():
 
     # Step 2: Analyze each day and store the results
     results = []
-    # Ensure we analyze dates within the 5-year window
-    # analysis_start_date = max(start_date, data['AAPL'].index[0])  # Use AAPL or another reliable stock
-    # analysis_dates = data['AAPL'].loc[analysis_start_date:].index
     analysis_dates = pd.date_range(
         start=start_date, 
         end=end_date, 
@@ -80,23 +77,7 @@ def main():
                 loser_open  = data[loser].loc[date, 'Open']
                 loser_close = data[loser].loc[date, 'Close']
                 loser_daily_change = (loser_close - loser_open) / loser_open * 100
-                # try:
-                #     stock = yf.Ticker(loser)
-                #     info = stock.info
-
-                #     # Check if data exists; sometimes it's missing, especially for less common stocks.
-                #     if "volume" in info and "marketCap" in info:
-                #         volume = info["volume"]
-                #         market_cap = info["marketCap"]
                         
-                #     else:
-                #         print(f"Volume or Market Cap data not available for {loser}")
-
-                #     time.sleep(1)
-                # except Exception as e:
-                #     print(f"Error fetching data for {loser}: {e}")
-                    
-            
 
                 results.append({
                     'date': date,
